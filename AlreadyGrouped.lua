56a2fea6e5b456f14d87330e9ae7125ebf1cac40
msturesson <msturesson@apple.com>
2026-01-07 23:33:47 +0000

    Initial commit: AlreadyGrouped

---
+++ AlreadyGrouped.lua
@@ -0,0 +1,20 @@
+local frame = CreateFrame("Frame")
+
+frame:RegisterEvent("CHAT_MSG_SYSTEM")
+
+frame:SetScript("OnEvent", function(_, _, message)
+    if not message then return end
+
+
+    -- Match: "<name> is already in a group"
+    local name = message:match("^(.+) is already in a group")
+
+    if name then
+        SendChatMessage(
+            "I tried inviting you but you're already in a group, you stupid bitch {rt8}",
+            "WHISPER",
+            nil,
+            name
+        )
+    end
+end)
